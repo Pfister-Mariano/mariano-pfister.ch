50a432254ea845e042148dfdb8fce226d2fdabe8
Mariano Pfister <mariano.pfister@bluewin.ch>
2024-06-05 09:12:37 +0200

    created the base modal box for the form

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
-mobileNavTrigger.onclick = function(){
+mobileNavTrigger.onclick = function () {
     document.body.classList.toggle('mobileNavActive')
 }
+
+let formTrigger = document.querySelectorAll('.formModalTrigger');
+formTrigger.forEach(function (elem) {
+    elem.addEventListener('click', function () {
+        this.classList.toggle('active')
+        document.querySelector('.formModal').classList.toggle('active')
+    })
+});
